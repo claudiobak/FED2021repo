be00c69b1a1d5ad8cc4738bca6b67901b6627776
claudiobak <claudiobakker@gmail.com>
2021-09-28 22:50:11 +0200

    Second commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,40 @@
 // JavaScript Document
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+        reveals[i].classList.add('active');
+    }
+    // else{
+    //     reveals[i].classList.remove('active');
+    // }
+  }
+}
+
+// const images = document.querySelector('.anim')
+
+// observer = new IntersectionObserver((entries) => {
+
+//     entries.forEach(entry => {
+//       if(entry.IntersectionRatio > 0) {
+//         entry.target.style.animation = `anim1 2s forwards ease-out` ;
+//       }
+//       else {
+//         entry.target.style.animation = 'none';
+//       }
+
+// })
+
+// images.forEach(image => {
+// observer.observe(images)
+// })
+// })
